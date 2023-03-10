0a1363f9411bd03f704d279409f747a6ed7b3b8f
timelic <i@timel.ink>
2023-03-10 16:16:38 +0800

    feat: installed hook

--- src/background/index.ts
+++ src/background/index.ts
@@ -2,6 +2,14 @@ export function dummy() {}
 
 console.log('hello world')
 
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.contextMenus.create({
+    title: 'My context Menu"',
+    contexts: ['page'],
+    id: 'myContextMenuId',
+  })
+})
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   const { menuItemId, selectionText } = info
   // @ts-ignore
