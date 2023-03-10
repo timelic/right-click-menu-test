f32898e5f20ea57f8f21d0d7fb3e37cc0d9d7b20
timelic <i@timel.ink>
2023-03-10 15:06:36 +0800

    feat: add right-click

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,11 @@
 export function dummy() {}
 
 console.log('hello world')
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  const { menuItemId, selectionText } = info
+  // @ts-ignore
+  const { id, url } = tab
+
+  console.log({ id, url, menuItemId, selectionText })
+})
